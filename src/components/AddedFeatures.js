5845ddad454afb0700ae4611ee49e2f29e610f7e
William Sulinski <w@williamsulinski.com>
2019-12-11 15:27:52 -0800

    MVP

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import AddedFeature from './AddedFeature';
 
@@ -6,17 +7,25 @@ const AddedFeatures = props => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {props.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
+          {props.features.map(item => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
-        <p>You can purchase items from the store.</p>
-      )}
+          <p>You can purchase items from the store.</p>
+        )}
     </div>
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = state => {
+  return {
+    length: state.car.features.length,
+    features: state.car.features
+  }
+}
+
+export default connect(mapStateToProps, {})(AddedFeatures);
+
